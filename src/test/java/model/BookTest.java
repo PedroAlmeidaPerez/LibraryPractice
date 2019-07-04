459844052279153deea7b8368668d02a369592d7
Pedro <pedro.almeida.perez@gmail.com>
2019-07-04 09:54:45 +0200

    method test getBook

--- src/test/java/model/BookTest.java
+++ src/test/java/model/BookTest.java
@@ -1,11 +1,23 @@
 package model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BookTest {
+    Book book = new Book("pedro", "juan");
+
     @Test
     public void testGetbook(){
+    String expected = "lolo";
+    String result = book.getName();
+        Assert.assertEquals(expected, result);
+    }
 
+    @Test
+    public void testGetbook2(){
+        String expected = "pedro";
+        String result = book.getName();
+        Assert.assertEquals(expected, result);
     }
 
 }
