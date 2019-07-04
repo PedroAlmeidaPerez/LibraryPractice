117cbda11efa3b95632c7a4ebdfdc05dfdb32d3f
Pedro <pedro.almeida.perez@gmail.com>
2019-07-04 15:31:38 +0200

    method test getID

--- src/test/java/model/BookTest.java
+++ src/test/java/model/BookTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class BookTest {
     Book book = new Book("pedro", "juan");
+    Book book2 = new Book("pedro", "juan");
 
     @Test
     public void testGetbook(){
@@ -19,5 +20,13 @@ public class BookTest {
         String result = book.getName();
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void testGetId(){
+        int expected = 1;
+        int result = book2.getIdBook();
+        Assert.assertEquals(expected, result);
+    }
+
 
 }
